6129cbc0e7dcdecb4243562560140fe3ea0010ed
Akshay <akshaypanampatta8891@gmail.com>
2023-08-09 00:35:35 +0530

    made navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -40,13 +40,12 @@ const Navbar = () => {
         </span>
 
         <span className={state === false ? "nav-right" : "nav-right1"}>
-          <ul className="right-list">
-            <li className="second-list discover">Discover</li>
-            <li className="second-list support">Support</li>
-            <li className="second-list search-icon"><SearchIcon/></li>
-            <li className="second-list cart-icon"><ShoppingCartIcon/></li>
-            <li className="second-list profile-icon"><PersonOutlineIcon/></li>
-           
+          <ul className={state===false?"right-list":"right-list-2"}>
+            <li className={state===false?"second-list discover":"second-list-2 "}>Discover</li>
+            <li className={state===false?"second-list support":"second-list-2 "}>Support</li>
+            <li className={state===false?"second-list search-icon":"second-list-2 "}><SearchIcon/></li>
+            <li className={state===false?"second-list cart-icon":"second-list-2 "}><ShoppingCartIcon/></li>
+            <li className={state===false?"second-list support profile-icon":"second-list-2 "}><PersonOutlineIcon/></li>
           </ul>
         </span>
         <span className="menubar" onClick={() => setState(!state)}>
