5e2e13204f579eeef2bec3198ff0dcdde40070c0
Akshay <akshaypanampatta8891@gmail.com>
2023-08-09 16:40:20 +0530

    footer added and made responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Home from "./Pages/Home"
+import "./App.css"
 
 
 function App() {
@@ -7,7 +8,6 @@ function App() {
   return (
     <>
       <Home/>
-      
     </>
   )
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,43 +1,70 @@
 import React from 'react'
 import "./Footer.css"
 
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+
+
 const Footer = () => {
   return (
     <>
       <div className="footer">
-        <div>
+        <div className="footer-row">
           <h3>Support</h3>
           <p>Online Help</p>
+          <p>Customer Service </p>
+          <p>Shipping FAQ</p>
           <p>Warranty</p>
           <p>Mi exchange</p>
-          <p>Customer Service</p>
+          <p>Bulk Orders</p>
+          <p>User Guide</p>
+          <p>Laptop Drivers</p>
+          <p>Mi Screen Protect</p>
+          <p>Mi Extended Wrranty</p>
+          <p>Mi Complete Project</p>
+          <p>Certification</p>
+          <p>Service Centre</p>
         </div>
-        <div>
+        <div className="footer-row">
           <h3>SHOP AND LEARN</h3>
-          <p>Phones</p>
+          <p>Redmi Phones</p>
           <p>Tv's</p>
           <p>Audio</p>
+          <p>Laptops and Tablets</p>
           <p>LifeStyle</p>
+          <p>Smart Home</p>
         </div>
-        <div>
+        <div className="footer-row">
           <h3>RETAIL STORE</h3>
           <p>Mi Store</p>
-          <p>Warranty</p>
-          <p>Mi Home</p>
+          <p>Mi Store Franchise</p>
           <p>Mi Authorised Store</p>
         </div>
-        <div>
+        <div className="footer-row">
           <h3>ABOUT US</h3>
+          <p>Xiaomi</p>
           <p>Privacy Policy</p>
           <p>User Agreement</p>
-          <p>Xiaomi</p>
+          <p>Integrity & Compliance</p>
+          <p>CSR and Disclosures</p>
+          <p>E-Waste Management</p>
+          <p>In The Press</p>
+          <p>Trust Center</p>
+          <p>Culture</p>
+          <p>SmartPhone Quality</p>
+          <p>TV Quality</p>
+          Service Quality
         </div>
-        <div>
+        <div className="footer-row">
           <h3>Follow Mi</h3>
-          <p>social media icons</p>
+          <FacebookIcon />
+          <TwitterIcon />
+          <InstagramIcon />
           
         </div>
       </div>
+      <hr />
     </>
   );
 }
